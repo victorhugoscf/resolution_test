dad968bafa0b429aa4737a555adc6d3277a925db
Victor H. Costa <101966095+victorhugoscf@users.noreply.github.com>
2025-06-06 07:24:31 -0300

    Update sale.blade.php

--- resources/views/pdf/sale.blade.php
+++ resources/views/pdf/sale.blade.php
@@ -95,7 +95,8 @@
                     <td>{{ ucfirst($payment->method) }}</td>
                     <td>R$ {{ number_format($payment->total_amount, 2, ',', '.') }}</td>
                 </tr>
-                @if (str_contains(strtolower($payment->method), 'credit') && $sale->installments->isNotEmpty())
+                @if (str_contains(strtolower($payment->method), 'credit') && $sale->installmentSales->isNotEmpty())
+
                     <tr>
                         <td colspan="2">
                             <table>
@@ -108,7 +109,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($sale->installments as $installment)
+                                    @foreach ($sale->installmentSales as $installment)
                                         <tr>
                                             <td>{{ $installment->installment_number }}</td>
                                             <td>R$ {{ number_format($installment->amount, 2, ',', '.') }}</td>
