f50880f4e8a4ad7a552bdfda0e04b35e4e6d1744
Victor H. Costa <101966095+victorhugoscf@users.noreply.github.com>
2025-06-11 03:10:17 -0300

    Update Section.blade.php

--- resources/views/layouts/Section.blade.php
+++ resources/views/layouts/Section.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Gestão de Vendas')</title>
-         <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" href="{{ asset('icon.png') }}" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
